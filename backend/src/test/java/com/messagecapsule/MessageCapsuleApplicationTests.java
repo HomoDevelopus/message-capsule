571658fccc9dd2e02c231150af46878073207627
yuri kim <63140456+yu-ri-kim@users.noreply.github.com>
2026-01-03 16:40:34 +0900

    Initial project setup: Spring Boot backend + React frontend

---
+++ backend/src/test/java/com/messagecapsule/MessageCapsuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.messagecapsule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageCapsuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
+
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Message Capsule</h1>
+    </div>
+  )
+}
+
+export default App
+
